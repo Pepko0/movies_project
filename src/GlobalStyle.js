d69b7f3301dcb5cc93499a02ab5a23b3fac9bac4
Kacper Lewko <k.lewkosga@gmail.com>
2024-03-22 11:21:54 +0100

    added new font-family

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -12,7 +12,7 @@ html {
       
       body {
         background: white;
-        font-family: ${({ theme }) => theme.font.inter};
+        font-family: Arial;
 
         @media(max-width: ${({ theme }) => theme.media.tablet}){
           padding:5px;
